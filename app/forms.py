90fdd04bf019d36d358c1622f45aaaf8d0efac7e
Erick Kamanda <info.proresearchers@gmail.com>
2024-08-29 12:46:26 +0300

    Create a RentalForm

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, DateTimeField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
+from models import Bicycle
 
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=45)])
@@ -14,3 +15,12 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=45)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=45)])
     submit = SubmitField('Sign In')
+
+class RentalForm(FlaskForm):
+    bicycle_id = SelectField('Bicycle', coerce=int, validators=[DataRequired()])
+    rental_time = DateTimeField('Rental Time', format='%Y-%m-%d %H:%M:%S', validators=[DataRequired()])
+    submit = SubmitField('Rent Bicycle')
+
+    def __init__(self, *args, **kwargs):
+        super(RentalForm, self).__init__(*args, **kwargs)
+        self.bicycle_id.choices = [(b.id, b.model) for b in Bicycle.query.filter_by(availability=True).all()]
